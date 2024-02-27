1371aa2bd6666385ebdf283e8a5a4793c166bf2f
Felippe Santana MAC <f-elipp-3@hotmail.com>
2024-02-27 21:42:49 +0000

    Login finalized, register section under construction...

--- website/views.py
+++ website/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    #Logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.error(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('home')
+    else:    
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.error(request, "You Have Been Logged Out...")
+    return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
